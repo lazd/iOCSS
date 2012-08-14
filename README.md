8f59dbd7e2283efad14636ad6d625042f73fbf6d
Larry Davis <lazd@lazd.net>
2012-08-14 01:11:01 -0700

    Initial commit; Chrome, Safari, iOS compatibility

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-iOCSS
-=====
+# iOCSS
+## Practical CSS for mobile applications.
 
-Simple, resolution independent, image-free CSS for mobile web applications
+Written with Stylus and easily customizable, iOCSS gives your app a "native" look and feel that closely resembles a popular mobile platform.
+
+## Get started
+
+1. Clone the repository
+
+```git clone https://github.com/lazd/iOCSS.git```
+
+2. Install the node modules
+
+```npm install```
+
+3. Build iOCSS
+
+```grunt```
+
+4. Build your app with the output file
+
+```cp build/css/io.css ~/myProject/css/```
---
+++ grunt.js
@@ -0,0 +1,35 @@
+/*global module:false*/
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-stylus');
+
+	// Project configuration.
+	grunt.initConfig({
+		meta: {
+			version: '0.1.0',
+			appName: 'iOCSS',
+			appWebSite: 'lazd.github.com/iocss'
+		},
+		stylus: {
+			compile: {
+				options: {
+					'paths': [
+					    'node_modules/',    // nib
+					    'styl/'             // Individual components
+					]
+				},
+				files: {
+					'build/css/io.css': 'styl/Main.styl'
+				}
+			}
+		},
+		watch: {
+			stylus: {
+				files: ['styl/*'],
+				tasks: 'stylus'
+			}
+		}
+	});
+    
+	// Default task
+	grunt.registerTask('default', 'stylus');
+};
